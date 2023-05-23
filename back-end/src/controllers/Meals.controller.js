f6b09957f382a9b81768a59eb8e56003cbe9aeed
Beatriz <beatriz.mediatto2@gmail.com>
2023-05-23 18:52:13 -0300

    feat: add getAll camada controller

--- back-end/src/controllers/Meals.controller.js
+++ back-end/src/controllers/Meals.controller.js
@@ -10,6 +10,16 @@ const getByName = async (req, res, next) => {
   }
 }
 
+const getAll = async (req, res, next) => {
+  try{
+    const result = await mealService.getAll();
+    return res.status(200).json(result);
+  } catch(error) {
+    next({...error, message: error.message, status: 404});
+  }
+}
+
 module.exports = {
   getByName,
+  getAll
 }
