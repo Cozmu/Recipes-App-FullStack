d6e225a9445c444be06b390aae1954166beda46e
Beatriz <beatriz.mediatto2@gmail.com>
2023-05-24 10:25:39 -0300

    fix: modifica função getByName e getAll de Drinks e melhora o retorno das funções

--- back-end/src/services/Drinks.service.js
+++ back-end/src/services/Drinks.service.js
@@ -1,17 +1,42 @@
-const { Drinks } =  require('../database/models/index');
+const { Drinks, DrinksIngredients } =  require('../database/models/index');
 
 const getByName = async (name) => {
-  const result = await Drinks.findOne({where: {str_drink: name}});
+  const result = await Drinks.findOne(
+    {
+      where: {str_drink: name},
+      include: [
+        { model: DrinksIngredients, as: 'drinksToIngredients' },
+      ],
+    }
+  );
 
   if(!result) throw new Error('Drink not found'); 
-
-  return result;
+  const resultJs = result.get();
+  const { drinksToIngredients } = resultJs;
+  delete resultJs.drinksToIngredients;
+  const resultWithOutAlias = {
+    ...resultJs,
+    ...drinksToIngredients.get()
+  }
+  return {drinks: resultWithOutAlias};
 }
 
 const getAll = async () => {
-  const result = await Drinks.findAll();
+  const result = await Drinks.findAll(
+    {
+      include: [
+        { model: DrinksIngredients, as: 'drinksToIngredients' },
+      ],
+    });
 
-  return result;
+  const resultJs = result.get();
+  const { drinksToIngredients } = resultJs;
+  delete resultJs.drinksToIngredients;
+  const resultWithOutAlias = {
+    ...resultJs,
+    ...drinksToIngredients.get()
+  }
+  return {drinks: resultWithOutAlias};
 }
 
 module.exports = {
