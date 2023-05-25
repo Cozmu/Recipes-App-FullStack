a06480db0fee43f25a8d58a925a0fb75843c67e8
victrosantos <victor.santos.fk@hotmail.com>
2023-05-25 20:16:17 -0300

    fix: consertando pro railway

--- front-end/src/utils/makeUrl.js
+++ front-end/src/utils/makeUrl.js
@@ -2,3 +2,4 @@ const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || 'http://';
 const HOST = process.env.REACT_APP_API_HOST || 'localhost:3001';
 const LOCAL = process.env.REACT_APP_API_HOST_LOCAL || 'localhost:3000';
 export { PROTOCOL, HOST, LOCAL };
+// x
